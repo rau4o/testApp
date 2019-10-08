4c17730a38035a35160276bacc186f0e2e9361fc
rau4o <rkoshkinbayev@gmail.com>
2019-10-08 13:16:50 +0600

    Initial Commit

---
+++ testApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by rau4o on 10/8/19.
+//  Copyright © 2019 rau4o. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
